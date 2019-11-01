d13c741e2e0ce4ecf8b8a10e1f6fd2fc66a2fc50
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-01 12:40:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,15 @@ class Dog
   @@all = []
   def initialize(name)
     @name = name 
-    @@all << 
+    @@all << self 
   end
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.print_all(all)
+    each @@all do |puppy|
+      puts puppy
+  end 
 end 
